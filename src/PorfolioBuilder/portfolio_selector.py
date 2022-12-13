3798b5bf6d9f35d94c3fae8743390154fc22beec
cmazzoni87 <cmazzoni87@aol.com>
2022-12-13 17:03:53 -0500

    int add

--- src/PorfolioBuilder/portfolio_selector.py
+++ src/PorfolioBuilder/portfolio_selector.py
@@ -7,7 +7,7 @@ def get_top_performers(df, n=10):
     Get the top performers in the portfolio.
     """
     # Get the top performers
-    industries =
+    industries = 1
     top_performers = df.sort_values(by='Return', ascending=False)[:n]
     # Get the top performers
     top_performers = top_performers.sort_values(by='Return', ascending=False)
